a90f2ea41a65e46b99bd16714b8305173b140f94
WolfDan <5377526+WolfDan@users.noreply.github.com>
2024-11-20 23:56:26 -0500

    fix git

--- programs/git.nix
+++ programs/git.nix
@@ -7,7 +7,10 @@
       enable = true;
       extraConfig = {
         credential = {
-          helper = "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe";
+          helper = "/mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager.exe";
+          "github.com" = {
+            usehttppath = true;
+          };
         };
       };
     };
